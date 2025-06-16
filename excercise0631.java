03fad9ebcb0ed77f98697d3445868e19906929d3
deziped <dezi.pedregon@gmail.com>
2025-06-16 18:50:47 -0400

    Update excercise0631.java

--- excercise0631.java
+++ excercise0631.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
-
 public class Exercise06_03 {
 
+
+
+    
     /** Return the reversal of an integer, e.g., reverse(456) returns 654 */
     public static int reverse(int number) {
         int reversed = 0;
@@ -13,23 +15,20 @@ public class Exercise06_03 {
         return reversed;
     }
 
-    /** Return true if number is a palindrome */
+    /** Return true if number is palindrome */
     public static boolean isPalindrome(int number) {
         return number == reverse(number);
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
+        Scanner input = new Scanner(System.in);      
         System.out.print("Enter a positive integer: ");
-        int number = input.nextInt();
-
+        int number = input.nextInt()
         if (isPalindrome(number)) {
             System.out.println(number + " is a palindrome");
         } else {
             System.out.println(number + " is not a palindrome");
         }
-
         input.close();
     }
 }
